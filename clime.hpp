b939e23cd99297c723f7b8f74f7f95755fbff0d1
Stefan Zipproth <info@zipproth.de>
2020-01-27 08:56:04 +0100

    added message_handler

--- clime.hpp
+++ clime.hpp
@@ -5,9 +5,12 @@
 #include <queue>
 #include <mutex>
 #include <condition_variable>
+#include <functional>
 #include <utility>
 #include <future>
 #include <chrono>
+#include <thread>
+#include <atomic>
 
 #if __cplusplus < 201402L
 #error You need at least C++14 for clime.hpp. Please check example/CMakeLists.txt on how to set compiler options. Reason: This code uses std::get<T> to extract the elements of a std::tuple whose type is T.
@@ -19,6 +22,20 @@ namespace clime
 	class message_manager
 	{
 	public:
+
+		virtual ~message_manager()
+		{
+			dispose();
+		}
+
+		void dispose()
+		{
+			{
+				std::lock_guard<std::mutex> lock(mutex_messages_);
+				running_ = false;
+			}
+			cv_.notify_one();
+		}
 		
 		template<typename MessageType>
 		void send_message(std::shared_ptr<MessageType> msg, unsigned int max_queued_messages=0)
@@ -28,12 +45,18 @@ namespace clime
 				std::unique_lock<std::mutex> lock(mutex_messages_);
 				cv_.wait(lock, [&]
 				{
-					return max_queued_messages==0 || std::get<QueueType>(messages_).size() < max_queued_messages;
+					return !running_ || max_queued_messages==0 || std::get<QueueType>(messages_).size() < max_queued_messages;
 				});
 				
 				std::get<QueueType>(messages_).emplace(msg);
 			}
 			cv_.notify_one();
+
+			using FunctionType = std::function<void(std::shared_ptr<MessageType>,bool)>;
+			if (std::get<FunctionType>(logger_) != nullptr)
+			{
+				std::get<FunctionType>(logger_)(msg, true);
+			}
 		}
 		
 		template<typename MessageType, typename Rep, typename Period>
@@ -56,7 +79,7 @@ namespace clime
 				std::unique_lock<std::mutex> lock(mutex_messages_);
 				cv_.wait(lock, [&]
 				{
-					return !wait_for_message || !std::get<QueueType>(messages_).empty();
+					return !running_ || !wait_for_message || !std::get<QueueType>(messages_).empty();
 				});
 				
 				auto& messages = std::get<QueueType>(messages_);
@@ -69,13 +92,138 @@ namespace clime
 			}
 			
 			cv_.notify_one();
+
+			using FunctionType = std::function<void(std::shared_ptr<MessageType>,bool)>;
+			if (std::get<FunctionType>(logger_) != nullptr)
+			{
+				std::get<FunctionType>(logger_)(result, false);
+			}
+
 			return result;
+		}
+
+		template<typename MessageType>
+		void set_logger(std::function<void(std::shared_ptr<MessageType>,bool)> logger)
+		{
+			using FunctionType = std::function<void(std::shared_ptr<MessageType>,bool)>;
+			std::get<FunctionType>(logger_) = logger;
 		}
 		
 	private:
 		std::tuple<std::queue<std::shared_ptr<MessageTypes>>...> messages_;
+		std::tuple<std::function<void(std::shared_ptr<MessageTypes>,bool)>...> logger_;
 		std::mutex mutex_messages_;
 		std::condition_variable cv_;
+		std::atomic<bool>running_{ true };
+	};
+
+	template<typename MessageType, typename MessageManagerType>
+	class message_handler
+	{
+	public:
+		message_handler(MessageManagerType& msg_manager,
+		                std::function<void(const MessageType& message_type)> on_message,
+		                std::function<void(const std::exception& exception)> on_exception=nullptr)
+		    : _msg_manager(msg_manager)
+			, on_exception_(on_exception)
+			, thread_(std::thread([=]
+			{
+				run(on_message, nullptr);
+			}))
+		{
+		}
+
+		message_handler(MessageManagerType& msg_manager,
+		                std::function<void(const MessageType& message_type)> on_message,
+						std::function<void()> on_idle,
+			            std::function<void(const std::exception& exception)> on_exception=nullptr)
+		    : _msg_manager(msg_manager)
+			, on_exception_(on_exception)
+			, thread_(std::thread([=]
+			{
+				run(on_message, on_idle);
+			}))
+		{
+		}
+
+		~message_handler()
+		{
+			try
+			{
+				dispose();
+				if (thread_.joinable())
+				{
+					thread_.join();
+				}
+			}
+			catch (const std::exception & ex)
+			{
+				if (on_exception_)
+				{
+					on_exception_(ex);
+				}
+			}
+		}
+
+		void dispose()
+		{
+			try
+			{
+				_msg_manager.dispose();
+				running_ = false;
+			}
+			catch (const std::exception& ex)
+			{
+				if (on_exception_)
+				{
+					on_exception_(ex);
+				}
+			}
+		}
+
+	private:
+		MessageManagerType& _msg_manager;
+		std::function<void(const std::exception& exception)> on_exception_;
+		std::thread thread_;
+		std::atomic<bool> running_{false};
+
+		void run(std::function<void(const MessageType& message_type)> on_message,
+				 std::function<void()> on_idle)
+		{
+			running_ = true;
+			const std::runtime_error unknown_exception("unknown exception");
+
+			while (running_)
+			{
+				try
+				{
+					auto incoming_message = _msg_manager.template receive_message<MessageType>(on_idle==nullptr);
+
+					if (incoming_message)
+					{
+						on_message(*incoming_message);
+					}
+					else
+					{
+						on_idle();
+					}
+				}
+				catch(const std::exception& ex)
+				{
+					if (on_exception_)
+					{
+						on_exception_(ex);
+					}
+				}
+				catch(...)
+				{
+					if (on_exception_)
+					{
+						on_exception_(unknown_exception);
+					}
+				}
+			}
+		}
 	};
 }
 
--- example/clime_example.cpp
+++ example/clime_example.cpp
@@ -1,8 +1,6 @@
 #include "clime.hpp"
-#include <thread>
-#include <atomic>
-#include <memory>
 #include <iostream>
+#include <list>
 #include <functional>
 #include <cmath>
 
@@ -15,7 +13,7 @@ public:
 	{
 	}
 
-	uint64_t get_number()
+	uint64_t get_number() const
 	{
 		return number_;
 	}
@@ -33,7 +31,7 @@ public:
 	{
 	}
 	
-	uint64_t get_prime()
+	uint64_t get_prime() const
 	{
 		return prime_number_;
 	}
@@ -42,102 +40,21 @@ private:
 	const uint64_t prime_number_;
 };
 
+bool is_prime(uint64_t p)
+{
+	if (p % 2 == 0) return false; // all prime numbers are odd
+
+	const uint64_t stop = static_cast<uint64_t>(std::sqrt(p));
+
+	for (uint64_t i = 3; i <= stop; i += 2)
+	{
+		if (p % i == 0) return false;
+	}
+
+	return true;
+}
+
 using message_manager_type = clime::message_manager<message_for_prime_checker, message_for_prime_printer>;
-
-// This class has two tasks:
-// (1) Sending potential prime numbers to one of the checker threads.
-// (2) Receiving messages from checker threads carrying confirmed primes, and printing them.
-class prime_printer
-{
-public:
-	explicit prime_printer(message_manager_type& message_manager)
-		: message_manager_(message_manager)
-	{
-	}
-	
-	void run(uint64_t start_prime)
-	{
-		// max_queued_messages avoids that this thread needlessly steals CPU time by sending messages that will never be received.
-		// This increases performance a bit, but requires correct order of shutdown of the threads - first this thread, then the checkers (workers).
-		// See note at the end of this file.
-		const unsigned int max_queued_messages = 100;
-		uint64_t p=start_prime;
-		
-		if (p%2==0) ++p; // make it odd, as we do not check even numbers if they are prime
-		
-		while (running)
-		{
-			// Create message to checker thread carrying the current potential prime numbe p. Then send it.
-			auto msg = std::make_shared<message_for_prime_checker>(p);
-			message_manager_.send_message(msg, max_queued_messages); // in case the message queue has grown to max_queued_messages, send_message will block until queue is shorter
-			
-			// Try to get a message from a checker thread that would confirm a number to be prime that we sent in the past.
-			// If there is no such message, it might be that no checker thread has finished yet, or we already processed all messaegs from checker threads.
-			auto message_for_us = message_manager_.receive_message<message_for_prime_printer>(false);
-			if (message_for_us)
-			{
-				std::cout << message_for_us->get_prime() << " ";
-			}
-
-			p += 2;
-		}
-	}
-
-	std::atomic<bool> running{true};
-	
-private:
-	message_manager_type& message_manager_;
-};
-
-// This class checks if numbers are prime and, if so, sends a message to the printer thread so it will printed.
-// The number we need to check arrives here as a message from another thread (the printer, which has two tasks: asking for checks and printing).
-class prime_checker
-{
-public:
-	explicit prime_checker(message_manager_type& message_manager)
-		: message_manager_(message_manager)
-	{
-	}
-	
-	void run()
-	{
-		while (running)
-		{
-			auto message_for_us = message_manager_.receive_message<message_for_prime_checker>(true);
-			if (message_for_us)
-			{
-				const uint64_t number_to_check = message_for_us->get_number();
-				
-				if (is_prime(number_to_check))
-				{
-					// We found that the number is prime, so send a message back to the printer.
-					auto msg = std::make_shared<message_for_prime_printer>(number_to_check);
-					message_manager_.send_message(msg);
-				}
-			}
-		}
-	}
-
-	std::atomic<bool> running{true};
-	
-private:
-	message_manager_type& message_manager_;
-	
-	static bool is_prime(uint64_t p)
-	{
-		if (p % 2 == 0) return false; // all prime numbers are odd
-
-		const uint64_t stop = static_cast<uint64_t>(std::sqrt(p));
-
-		for (uint64_t i = 3; i <= stop; i += 2)
-		{
-			if (p % i == 0) return false;
-		}
-
-		return true;
-	}
-};
-
 
 int main(int argc, char**argv)
 {
@@ -155,28 +72,43 @@ int main(int argc, char**argv)
 	const int        n_threads = std::atoi(argv[3]);
 	
 	message_manager_type message_manager;
-	
-	std::vector<std::shared_ptr<prime_checker>> prime_checkers;
-	std::vector<std::shared_ptr<std::thread>> prime_checker_threads;
-	
-	for (int i = 0; i < n_threads; ++i)
+
+	// just a sample how logging is possible - We do not really log in this sample.
+	message_manager.set_logger<message_for_prime_checker>([](std::shared_ptr<message_for_prime_checker> msg, bool sending)
 	{
-		prime_checkers.emplace_back(std::make_shared<prime_checker>(message_manager));
-		prime_checker_threads.emplace_back(std::make_shared<std::thread>([&]{prime_checkers.back()->run();}));
-	}
+		// Each and every message_for_prime_checker that is sent will arrive here, so this can be used for easy logging of one or all message types.
+	});
+
+	uint64_t p = start_prime + (start_prime%2==0); // make it odd, as we iterate with step size 2
+
+	std::list<std::shared_ptr<clime::message_handler<message_for_prime_checker, message_manager_type>>> prime_checker_handlers;
 	
-	prime_printer printer(message_manager);
-	std::thread prime_printer_thread = std::thread([&]{printer.run(start_prime);});
+	clime::message_handler<message_for_prime_printer, message_manager_type> handle_msg_for_prime_printer(message_manager, [](const message_for_prime_printer& msg)
+	{
+		std::cout << msg.get_prime() << " ";
+	}, [&]() // handler for no message from prime checker: we send requests to the prime checker
+	{
+		auto msg = std::make_shared<message_for_prime_checker>(p);
+		message_manager.send_message(msg, 100); // in case prime checker's message queue has reached 100 messages, wait until it processed one
+		p += 2; // proceed to the next odd number to check if it is prime
+	});
+
+	for (int i = 0; i < n_threads; i++)
+	{
+		prime_checker_handlers.emplace_back(std::make_shared<clime::message_handler<message_for_prime_checker, message_manager_type>>(message_manager, [&](const message_for_prime_checker& msg)
+		{
+			const uint64_t number_to_check = msg.get_number();
+
+			if (is_prime(number_to_check))
+			{
+				// We found that the number is prime, so send a message back to the printer.
+				auto msg = std::make_shared<message_for_prime_printer>(number_to_check);
+				message_manager.send_message(msg);
+			}
+		}));
+	}
 
 	std::this_thread::sleep_for(std::chrono::seconds(time_limit));
-
-	// Stop threads - first the printer, because it may be waiting for checkers to consume messages.
-	// If we would stop the worker threads first, the printer might be waiting indefinitely that the message queue becomes shorter.
-	printer.running = false;
-	prime_printer_thread.join();
-
-	for (int i = 0; i < n_threads; ++i) prime_checkers[i]->running = false;
-	for (int i = 0; i < n_threads; ++i) prime_checker_threads[i]->join();
 
 	return 0;
 }
